d1a9005f920adef24cc74637aa51c17f40c768c2
hnu <tindecken@gmail.com>
2025-10-22 10:46:38 +0700

    chore(windows): Update Windows project configuration and branding
    - Rename application binary from "google_spreadsheet_flutter" to "d3"
    - Update company name from "com.example" to "Tind"
    - Modify default window size from 1280x720 to 380x670
    - Replace application icon with new design
    - Update legal copyright information
    - Adjust window title to "D3"
    Reflects rebranding and configuration changes for Windows platform deployment.

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,8 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"google_spreadsheet_flutter", origin, size)) {
+  Win32Window::Size size(380, 670);
+  if (!window.Create(L"D3", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
